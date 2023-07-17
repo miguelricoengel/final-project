a469415370ea868b66c6dfc4d1c4b2f4d7cac815
Norbert Busom <norbertbg@outlook.com>
2023-07-17 20:18:06 +0200

    dashlist

--- src/components/DashList.jsx
+++ src/components/DashList.jsx
@@ -2,17 +2,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import DashListItem from "./DashListItem";
 
-const API_URL = "/backend";
-
 function DashList() {
 
-  const API_URL = "/backend";
+  const API_URL = '/backend';
   const [Dashes, setDashes] = useState([]);
   const storedToken = localStorage.getItem("authToken");
 
   const getAllDashes = () => {
     axios
-      .get(`${API_URL}/api/`,
+      .get(`${API_URL}/api/dash`,
       { headers: { Authorization: `Bearer ${storedToken}` } })
       .then((response) => setDashes(response.data))
       .catch((error) => console.log(error));
