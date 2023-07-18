86db66032fb5d4d4f67617f9ab8ebc3cf525cb11
Norbert Busom <norbertbg@outlook.com>
2023-07-18 14:08:56 +0200

    home with referral code implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Buble from "../components/Buble";
 import DashList from "../components/DashList";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
+import axios from "axios";
+
+const API_URL = '/backend';
 
 function Home() {
   const { user } = useContext(AuthContext);
+
+  const [dashboardId, setDashboardId] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+  const storedToken = localStorage.getItem("authToken");
+  const handleCode = (e) => { setDashboardId(e.target.value) };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const requestBody = { dashboardId };
+    console.log(requestBody)
+
+    axios.put(
+      `${API_URL}/api/referral-code`, requestBody,
+      {
+        headers: {
+          Authorization: `Bearer ${storedToken}`
+        }
+      }
+    )
+      .then(() => {
+        navigate(`/${dashboardId}`);
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  }
+
   return (
     <div id="flex">
       <h2 className="m-3 flex items-start p-3 text-xl">Home</h2>
@@ -25,11 +59,36 @@ function Home() {
           <label className="flex">create new Dash</label>
         </Link>
       </div>
-      <div className="flex items-end justify-end">
-        <Link to="/:dashID/settings">
+      <div className=" flex items-end justify-end">
           {" "}
-          <Buble text={<p className="text-sm">settings</p>} size="small" />{" "}
-        </Link>
+          <Buble 
+          text={
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="dashboardId">Code</label>
+                <input
+                  type="text"
+                  className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm text-white outline-none transition duration-150 ease-in-out"
+                  id="dashboardId"
+                  placeholder="Your Code"
+                  value={dashboardId}
+                  onChange={handleCode}
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="bg-gray focus:border-blue rounded-full border-[#38bcf9] px-4 py-2 text-sm text-[#38bcf9] focus:outline-none"
+                >
+                  Submit
+                </button>
+              </div>
+               </form>
+          }
+                
+              />{" "}
+          
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
       </div>
     </div>
   );
