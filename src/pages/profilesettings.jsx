5b1ffdc61841ca5eee545ad3f901d6e0010db115
Norbert Busom <norbertbg@outlook.com>
2023-07-19 00:57:12 +0200

    profile settings styled

--- src/pages/profilesettings.jsx
+++ src/pages/profilesettings.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 import Buble from "../components/Buble";
@@ -13,6 +13,7 @@ function ProfileSettings(image) {
   const storedToken = localStorage.getItem("authToken");
 
   const navigate = useNavigate();
+  const fileInputRef = useRef(null);
 
   function handleImageError() {
     setImageSrc("/pics/Bubble_Pose.png");
@@ -24,6 +25,10 @@ function ProfileSettings(image) {
 
   const handleProfileImage = (e) => {
     setProfileImage(e.target.files[0]);
+  };
+
+  const handleChooseFile = () => {
+    fileInputRef.current.click();
   };
 
   const handleSubmit = (e) => {
@@ -51,39 +56,39 @@ function ProfileSettings(image) {
   };
   return (
     <div>
-      <h2>Profile Settings</h2>
+      <h2 className="text-lg font-bold mb-4">Profile Settings</h2>
       <br />
       <br />
       <div className="flex">
         <Buble
-        text={
-          <div>
-            {imageSrc ? (
-              <img
-                className="h-full w-full rounded-full p-16"
-                src={imageSrc}
-                alt="image"
-                onError={handleImageError}
-              />
-            ) : (
-              <img
-                className="h-12 w-12 rounded-full border-2 border-blue-200 p-1"
-                src="/pics/Bubble_Pose.png"
-                alt="default image"
-              />
-            )}
-          </div>
-        }
-        className="w-40"
-        size="medium"
-      />
+          text={
+            <div>
+              {imageSrc ? (
+                <img
+                  className="h-full w-full rounded-full p-16"
+                  src={imageSrc}
+                  alt="image"
+                  onError={handleImageError}
+                />
+              ) : (
+                <img
+                  className="h-12 w-12 rounded-full border-2 border-blue-200 p-1"
+                  src="/pics/Bubble_Pose.png"
+                  alt="default image"
+                />
+              )}
+            </div>
+          }
+          className="w-40"
+          size="medium"
+        />
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="space-y-4 md:w-1/2 lg:w-2/3 xl:w-1/2 mx-auto">
         <div>
           <label htmlFor="email">Email</label>
           <input
             type="text"
-            className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
+            className="w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
             id="email"
             placeholder={user.email}
             readOnly
@@ -93,7 +98,7 @@ function ProfileSettings(image) {
           <label htmlFor="userName">UserName</label>
           <input
             type="userName"
-            className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
+            className="w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
             id="userName"
             placeholder={user.userName}
             value={userName}
@@ -104,7 +109,7 @@ function ProfileSettings(image) {
           <label htmlFor="password">Password</label>
           <input
             type="password"
-            className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
+            className="w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
             id="password"
             placeholder="******"
             readOnly
@@ -114,15 +119,23 @@ function ProfileSettings(image) {
           <label htmlFor="profileImage">Photo</label>
           <input
             type="file"
-            className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out"
             id="profileImage"
             onChange={handleProfileImage}
+            ref={fileInputRef}
+            className="hidden"
           />
+          <button
+            type="button"
+            onClick={handleChooseFile}
+            className="w-full rounded-full text-gray-400 border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out hover:bg-[#38bcf9] hover:bg-opacity-50 focus:bg-[#38bcf9] focus:bg-opacity-50"
+          >
+            Choose File
+          </button>
         </div>
         <div>
           <button
             type="submit"
-            className="mb-4 w-full rounded-full border bg-[#38bcf9] bg-opacity-25 p-2 text-sm outline-none transition duration-150 ease-in-out text-blue-500"
+            className="bg-gray focus:border-blue rounded-full border-[#38bcf9] px-4 py-2 text-sm text-blue-700 focus:outline-none"
           >
             Update
           </button>
