e9ada1f8abd46488b24ed23b53cef054d38c78c1
Mike <ricoengelmiguel@gmail.com>
2023-07-26 00:32:24 +0200

    try 3 for final push for deploy

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,8 +1,5 @@
-
 function Messages(quote) {
-
   function formatCreatedAt(createdAt) {
-   
     const date = new Date(createdAt);
 
     const options = {
@@ -19,14 +16,18 @@ function Messages(quote) {
     };
   }
 
-  const authorInfo = formatCreatedAt(quote.createdAt);
+  const authorInfo = typeof quote.createdAt === 'string' ? formatCreatedAt(quote.createdAt) : null;
 
   return (
     <div className="scrollbar-track-blue-300 scrollbar-thumb-blue-500 w-full overflow-y-scroll p-5">
-     <div className="rounded-lg border-4 border-blue-200 p-3">
-  <p className="whitespace-normal">{quote.text}</p>
-</div>
-  
+      <div className="rounded-lg border-4 border-blue-200 p-3">
+        <p className="whitespace-normal">{quote.text}</p>
+        {authorInfo && (
+          <p className="text-gray-500">
+            Posted by {authorInfo.userName} on {authorInfo.date}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
