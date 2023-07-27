5474ca5f65007176493cc578f1b2e712f8fef2d6
Norbert Busom <norbertbg@outlook.com>
2023-07-27 19:00:16 +0200

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: 'https://yourbuble.netlify.app//',
   plugins: [react()],
   server: {
     proxy: {
       '/backend': { 
-        target: 'apibuble.fly.dev',
+        target: 'https://apibuble.fly.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/backend/, ''),
       },
